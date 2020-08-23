2a3bd9a7610b044d03c91b85a2a273935f8e574d
tuananhcntt2k58utc <tuananhk58utc@gmail.com>
2020-08-23 17:14:33 +0700

    'addFuntionEatMouse'

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,13 @@
 function Mouse(){
 	this.name = name;
 	this.dead = false;
+	this.stomach = [];
 }
 
 Mouse.prototype.die = function(){
 	this.dead = true;
+}
+
+Mouse.prototype.eat = function(a){
+	this.stomach.push(a)
 }
